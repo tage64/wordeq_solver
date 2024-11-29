6e78662b155abb501c2dcc5cd4cf5432d77cf74a
Tage Johansson <tajo9185@student.uu.se>
2024-11-29 17:44:04 +0100

    Change slurm parallelism script.

--- slurm_scripts/benchmark_parallelism.sh
+++ slurm_scripts/benchmark_parallelism.sh
@@ -4,13 +4,12 @@
 #SBATCH -t 00:04:00
 #SBATCH -J str_solver_benchmark_parallelism
 
-cargo r -r -- -p1 -vt10 --only 3 benchmark b2
-cargo r -r -- -p2 -vt10 --only 3 benchmark b2
-cargo r -r -- -p4 -vt10 --only 3 benchmark b2
 cargo r -r -- -p8 -vt10 --only 3 benchmark b2
+cargo r -r -- -p9 -vt10 --only 3 benchmark b2
+cargo r -r -- -p10 -vt10 --only 3 benchmark b2
+cargo r -r -- -p11 -vt10 --only 3 benchmark b2
+cargo r -r -- -p12 -vt10 --only 3 benchmark b2
+cargo r -r -- -p13 -vt10 --only 3 benchmark b2
+cargo r -r -- -p14 -vt10 --only 3 benchmark b2
+cargo r -r -- -p15 -vt10 --only 3 benchmark b2
 cargo r -r -- -p16 -vt10 --only 3 benchmark b2
-cargo r -r -- -p20 -vt10 --only 3 benchmark b2
-cargo r -r -- -p24 -vt10 --only 3 benchmark b2
-cargo r -r -- -p32 -vt10 --only 3 benchmark b2
-cargo r -r -- -p40 -vt10 --only 3 benchmark b2
-cargo r -r -- -p41 -vt10 --only 3 benchmark b2
